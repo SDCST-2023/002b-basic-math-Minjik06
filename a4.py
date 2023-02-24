232006821f3d75bba9a618b7871ab78e294155c6
Minjik06 <minjik525@deltalearns.ca>
2023-02-24 10:19:01 -0800

    assignment4

---
+++ a4.py
@@ -0,0 +1,10 @@
+"""Copy the contents of a2.py and create a new file called a4.py that has the same content.
+There are two variables that have assigned values for x and y
+Display only the quotient of the two numbers (use division)
+The answer should be calculated and stored in a variable called "answer"
+(1 points)"""
+
+x = 8
+y= 5.0
+answer = x/y
+print((int)(answer))
